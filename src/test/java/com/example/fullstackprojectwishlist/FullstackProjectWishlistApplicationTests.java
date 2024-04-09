d9e874b5bca87bb494624e3c6accf755a28c3afc
David Lu <davidlu1999@gmail.com>
2024-04-09 13:16:42 +0200

    initial commit

---
+++ src/test/java/com/example/fullstackprojectwishlist/FullstackProjectWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackprojectwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
