1ea60ed482cb76041433432536f9ec619277bb24
bigcheesespaghett <134272992+bigcheesespaghett@users.noreply.github.com>
2024-04-10 12:20:57 +0200

    First controller and model classes

---
+++ src/main/java/controllers/WishListController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
+@Controller
+public class WishListController {
+
+    @Autowired
+    private WishlistService wishlistService;
+
+    @GetMapping("/wishlist")
+    public String viewWishlist(Model model) {
+        // lav metode i wishlist
+        List<Wishlist> wishlists = wishlistService.getAllWishlists();
+        model.addAttribute("wishlists", wishlists);
+        return "wishlist";
+    }
+
+    @PostMapping("/wishlist/add")
+    public String addWishlist(@RequestParam int userId, @RequestParam String wishlistName) {
+        // Lad addwishlist metode
+        wishlistService.addWishlist(userId, wishlistName);
+        return "redirect:/wishlist";
+    }
+
+    @PostMapping("/wishlist/delete")
+    public String deleteWishlist(@RequestParam int wishlistId) {
+        // lav metode i wishlist
+        wishlistService.deleteWishlist(wishlistId);
+        return "redirect:/wishlist";
+    }
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,58 @@
+package models;
+
+public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(int userId, String firstName, String lastName, String email){
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAuthenticationCode() {
+        return authenticationCode;
+    }
+
+    public void setAuthenticationCode(String authenticationCode) {
+        this.authenticationCode = authenticationCode;
+    }
+
+    private String authenticationCode;
+
+}
