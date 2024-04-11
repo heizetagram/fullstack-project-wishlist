696511411a588d48b639ba1e3102cc6a3787012d
David Lu <davidlu1999@gmail.com>
2024-04-11 19:44:47 +0200

    Updated

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package repositories;
 
+import models.User;
+import models.Wish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,15 +12,42 @@ import java.util.List;
 
 @Repository
 public class UserRepository {
-    /*
+
     @Autowired
     JdbcTemplate jdbcTemplate;
-    public void addUser(int userId, String firstName, String lastName, String email, String user_password); {
-        String query = "Insert INTO user(int userId, String firstName, String lastName, String email, String user_password)" +
+
+    // Add user
+    public void addUser(String firstName, String lastName, String email, String userPassword) {
+        String query = "Insert INTO user(String firstName, String lastName, String email, String userPassword)" +
                 "VALUES (?, ?, ?, ?, ?)";
-        jdbcTemplate.update()
+        jdbcTemplate.update(query, firstName, lastName, email, userPassword);
     }
 
-     */
-}
 
+    public User getUserById(int userId) {
+        String query = "SELECT * FROM user WHERE user_Id = ?;";
+        return jdbcTemplate.queryForObject(query, User.class, userId);
+    }
+
+    public void updateUserById(int userId, String firstName, String lastName, String email, String userPassword) {
+        String query = "UPDATE user " +
+                "SET userId = ? " +
+                "first_name = ?" +
+                "last_name = ?" +
+                "email = ?" +
+                "user_password = ?" +
+                "WHERE userId = ?";
+        jdbcTemplate.update(query, userId, firstName, lastName, email, userPassword);
+    }
+
+    public List<User> getUsers() {
+        String query = "SELECT  * FROM user";
+        RowMapper rowMapper = new BeanPropertyRowMapper(User.class);
+        return jdbcTemplate.query(query, rowMapper);
+    }
+
+    public void deleteUser(int userId) {
+        String query = "DELETE FROM user WHERE userId = ?";
+        jdbcTemplate.update(query, userId);
+    }
+}
